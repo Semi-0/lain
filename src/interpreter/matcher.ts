b63c7a70673513df3db181380ab9872ff8e4f927
semi <gtlpd21@gmail.com>
2024-11-05 15:46:10 +0100

    matcher now supports return new applicability

--- src/interpreter/matcher.ts
+++ src/interpreter/matcher.ts
@@ -2,7 +2,7 @@ import { isSucceed } from "pmatcher/Predicates"
 import { match } from "pmatcher/MatchBuilder"
 import { construct_advice, install_advice } from "generic-handler/built_in_generics/generic_advice"
 import type { MatchResult } from "pmatcher/MatchResult/MatchResult"
-import { register_predicate } from "generic-handler/Predicates"
+import { match_args, register_predicate } from "generic-handler/Predicates"
 import { to_string } from "generic-handler/built_in_generics/generic_conversation"
 import { define_generic_procedure_handler } from "generic-handler/GenericProcedure"
 
@@ -10,20 +10,17 @@ function no_change(a: any) {
     return a
 }
 
-
 export function matcher_advice() : any[]{
     var matchResult: MatchResult | null = null 
 
     const input_modifers =  [no_change,
                              (expr: any[]) => { 
-                                return (input: any[], ...args: any[]) => {
-                                    const matcher = register_predicate(to_string(expr), (input: any[]) => {
-                                        matchResult = match(input, expr)
-                                        return isSucceed(matchResult)
-                                    })
-                                    
-                                    return matcher(input)
-                                }
+                                const matcher = register_predicate(to_string(expr), (input: any[]) => {
+                                    matchResult = match(input, expr)
+                                    return isSucceed(matchResult)
+                                })
+                                
+                                return match_args(matcher)
                               },
                               (handler: (exec: (...args: any[]) => any, ...args: any[]) => any) => { 
                                 return (result: any, ...args: any[]) => {
@@ -33,4 +30,4 @@ export function matcher_advice() : any[]{
     return construct_advice(input_modifers, no_change)
 }
 
-export const generic_matcher = install_advice(define_generic_procedure_handler, matcher_advice)
+export const define_generic_matcher = install_advice(define_generic_procedure_handler, matcher_advice)
